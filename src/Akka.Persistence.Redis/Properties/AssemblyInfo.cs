8de724840323992181e69bbcf31914678c7e46e9
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-02-10 21:15:38 +0700

    Add Redis Cluster RedisKey distribution unit test (#134)
    * Add Redis Cluster RedisKey distribution unit test
    
    Co-authored-by: Aaron Stannard <aaron@petabridge.com>

--- src/Akka.Persistence.Redis/Properties/AssemblyInfo.cs
+++ src/Akka.Persistence.Redis/Properties/AssemblyInfo.cs
@@ -9,4 +9,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("Akka.Persistence.Redis.Tests")]
+[assembly: InternalsVisibleTo("Akka.Persistence.Redis.Cluster.Tests")]
 [assembly: InternalsVisibleTo("Akka.Persistence.Redis.Benchmarks")]
